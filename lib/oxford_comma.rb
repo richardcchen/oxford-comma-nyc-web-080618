ec4f59039b521deb0d7cc26fd77ca67418a8c690
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 02:02:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  
+  else    
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
