380e219ddd285470b05c7f129828e2df7a7a3414
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 02:00:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,8 @@ def oxford_comma(array)
   if array.size == 1
     return array[0]
   
-  elseif array.length == 2
-    temp = array[0]
-    return temp
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
   
   else    
     #array[-1] = "and #{array[-1]}"
